38462f9a4a0c7c96f1a61977cd63d4aee63a34a9
hkoon1 <97872389+hkoon1@users.noreply.github.com>
2022-01-16 23:01:50 -0800

    Update UploadDocs.cmd

--- 22-Create-a-search-solution/UploadDocs.cmd
+++ 22-Create-a-search-solution/UploadDocs.cmd
@@ -2,9 +2,9 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 rem Set values for your storage account
-set subscription_id=YOUR_SUBSCRIPTION_ID
-set azure_storage_account=YOUR_AZURE_STORAGE_ACCOUNT_NAME
-set azure_storage_key=YOUR_AZURE_STORAGE_KEY
+set subscription_id=afc6f403-3bed-4dc5-bcef-34c7a275af68
+set azure_storage_account=trystorageaccount01
+set azure_storage_key=MztmLZM8WyecE3bzMBQpekqACmGaErxdpB+N3xx64nnwQ897wXFEWff10I5aPnJHjEDzuSj7t//YZoyfux+/5g==
 
 
 echo Creating container...
